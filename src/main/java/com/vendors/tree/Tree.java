b960929cb1a78a6c268e52e6c0341a5df6e6efc9
moises-herrera <moisesherrera037@gmail.com>
2022-05-23 09:56:54 -0500

    add multiple children to tree node

--- src/main/java/com/vendors/tree/Tree.java
+++ src/main/java/com/vendors/tree/Tree.java
@@ -1,5 +1,55 @@
 package main.java.com.vendors.tree;
 
+import main.java.com.vendors.list.List;
+import main.java.com.vendors.list.ListNode;
+import main.java.com.vendors.models.Vendor;
+
 public class Tree {
+    private TreeNode root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public void insert(Vendor data, long parentId) {
+        TreeNode newNode = new TreeNode(data);
+        if (root == null) {
+            root = newNode;
+        } else if (parentId > 0) {
+            if (root.getVendor().getCedula() == parentId) {
+                root.addChild(newNode);
+                return;
+            }
+
+            TreeNode parent = findParentById(root.getChildren(), parentId);
+            if (parent != null) parent.addChild(newNode);
+        }
+    }
+
+    public TreeNode findParentById(List children, long parentId) {
+        ListNode current = children.getHead();
+
+        while (current != null) {
+            if (current.getTreeNode().getVendor().getCedula() == parentId) {
+                break;
+            }
+
+            if (!current.getTreeNode().getChildren().isEmpty()) {
+                return findParentById(current.getTreeNode().getChildren(), parentId);
+            }
+
+            current = current.getNext();
+        }
+
+        return current != null ? current.getTreeNode() : null;
+    }
 
 }
--- src/main/java/com/vendors/tree/TreeNode.java
+++ src/main/java/com/vendors/tree/TreeNode.java
@@ -4,11 +4,31 @@ import main.java.com.vendors.list.List;
 import main.java.com.vendors.models.Vendor;
 
 public class TreeNode {
-    public Vendor vendor;
-    public List children;
+    private Vendor vendor;
+    private List children;
 
     public TreeNode (Vendor data) {
         vendor = data;
         children = new List();
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
+
+    public List getChildren() {
+        return children;
+    }
+
+    public void setChildren(List children) {
+        this.children = children;
+    }
+
+    public void addChild(TreeNode node) {
+        this.children.add(node);
     }
 }
